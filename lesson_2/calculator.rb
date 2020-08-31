4e32c4d15cebc41f9de873c7e16e8bab1c8539c2
Anne Tiotuico <amtiotuico@gmail.com>
2020-08-31 02:16:31 +0000

    moved messages into configuration file

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,37 +1,39 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
+# methods
 def prompt(message)
   puts("=> #{message}")
 end
 
 def number?(num)
-  num.to_i.to_s == num || num.to_f.to_s == num 
+  num.to_i.to_s == num || num.to_f.to_s == num
 end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  word = case op
+         when '1'
+           'Adding'
+         when '2'
+           'Subtracting'
+         when '3'
+           'Multiplying'
+         when '4'
+           'Dividing'
+         end
+  word
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+# main program
+
+prompt MESSAGES['welcome']
 
 name = ''
 loop do
   name = gets.chomp
-  
+
   if name.empty?
-    prompt "Make sure to use a valid name."
+    prompt MESSAGES['valid_name']
   else
     break
   end
@@ -42,50 +44,50 @@ prompt("Hi #{name}!")
 loop do # main loop
   number1 = ''
   loop do
-    prompt "What's the first number?"
+    prompt MESSAGES['first_number']
     number1 = gets.chomp
-    
+
     if number?(number1)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt MESSAGES['not_valid_number']
     end
   end
-  
+
   number2 = ''
   loop do
-    prompt "What's the second number?"
+    prompt MESSAGES['second_number']
     number2 = gets.chomp
-    
+
     if number?(number2)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt MESSAGES['not_valid_number']
     end
   end
-  
+
   operator_prompt = <<-MSG
-    What operation would you like for perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
+  What operation would you like for perform?
+      1) add
+      2) subtract
+      3) multiply
+      4) divide
   MSG
-    
+
   prompt operator_prompt
   operator = ''
   loop do
     operator = gets.chomp
-    
+
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt "Must choose 1, 2, 3, or 4"
+      prompt MESSAGES['valid_operator']
     end
   end
-  
+
   prompt "#{operation_to_message(operator)} the two numbers..."
-  
+
   result = case operator
            when '1'
              number1.to_f + number2.to_f
@@ -96,13 +98,12 @@ loop do # main loop
            when '4'
              number1.to_f / number2.to_f
            end
-  
+
   prompt "The result is #{result}"
-  
-  prompt "Do you want to peform another calculation? (Y to calculate again)"
+
+  prompt MESSAGES['another_calculation']
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-  
 end
 
-prompt "Thank you for using the calculator. Good bye!"
+prompt MESSAGES['goodbye']
