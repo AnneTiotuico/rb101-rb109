3f47de44ca336543902eae9247998a5586e71144
Anne Tiotuico <amtiotuico@gmail.com>
2020-09-03 03:15:15 +0000

    Refactored methods and made prompts more clear

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -11,10 +11,10 @@ def prompt(key, lang)
   puts("=> #{message}")
 end
 
-def get_lang()
+def get_lang
   lang_choice = ''
   loop do
-    puts messages('language', lang='en')
+    puts messages('language', 'en')
     lang_choice = gets.chomp
     break if %w(en es fil).include?(lang_choice)
   end
@@ -24,12 +24,12 @@ end
 def get_name(lang)
   name = ''
   loop do
-  name = gets.chomp
+    name = gets.chomp
 
-    unless /^[[:alpha:]]+$/.match(name)
-      prompt 'invalid_name', lang
-    else
+    if /^[[:alpha:]]+$/.match(name) || /^[[:alpha:]]+ [[:alpha:]]+$/.match(name)
       break
+    else
+      prompt 'invalid_name', lang
     end
   end
   name
@@ -39,17 +39,47 @@ def number?(num)
   num.to_i.to_s == num || num.to_f.to_s == num
 end
 
+def get_num(num_order, lang)
+  number = ''
+  loop do
+    prompt num_order, lang
+    number = gets.chomp
+
+    if number?(number)
+      break
+    else
+      prompt 'not_valid_number', lang
+    end
+  end
+  number
+end
+
+def get_operator(lang, number2)
+  operator = ''
+  loop do
+    operator = gets.chomp
+    if operator == '4' && number2 == '0'
+      prompt 'zero_division_error', lang
+    elsif %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt 'valid_operator', lang
+    end
+  end
+  operator
+end
+
 def operation_to_message(op, lang)
   operation = case op
-         when '1'
-          messages('adding', lang)
-         when '2'
-          messages('subtracting', lang)
-         when '3'
-          messages('multiplying', lang)
-         when '4'
-          messages('dividing', lang)
-         end
+              when '1'
+                messages('adding', lang)
+              when '2'
+                messages('subtracting', lang)
+              when '3'
+                messages('multiplying', lang)
+              when '4'
+                messages('dividing', lang)
+              end
   operation
 end
 
@@ -64,46 +94,13 @@ name = get_name(lang)
 
 puts format(messages('greeting', lang), name: name)
 
-
 #  ------------ main loop ------------
 loop do
-  number1 = ''
-  loop do
-    prompt 'first_number', lang
-    number1 = gets.chomp
-
-    if number?(number1)
-      break
-    else
-      prompt 'not_valid_number', lang
-    end
-  end
-
-  number2 = ''
-  loop do
-    prompt 'second_number', lang
-    number2 = gets.chomp
-
-    if number?(number2)
-      break
-    else
-      prompt 'not_valid_number', lang
-    end
-  end
+  number1 = get_num('first_number', lang)
+  number2 = get_num('second_number', lang)
 
   prompt 'operator_prompt', lang
-  operator = ''
-  loop do
-    operator = gets.chomp
-
-    if operator == '4' && number2 == '0'
-      prompt 'zero_division_error', lang
-    elsif %w(1 2 3 4).include?(operator)
-      break
-    else
-      prompt 'valid_operator', lang
-    end
-  end
+  operator = get_operator(lang, number2)
 
   puts format(messages('operation', lang),
               operation: operation_to_message(operator, lang),
