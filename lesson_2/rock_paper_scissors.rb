0a75681831721222ed75e0b0baa6e267518595ca
Anne Tiotuico <amtiotuico@gmail.com>
2020-09-09 22:22:40 +0000

    added lizard and spock options

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,15 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = { "rock" => ['scissors', 'lizard'],
+                  "paper" => ['rock', 'spock'],
+                  "scissors" => ['paper', 'lizard'],
+                  "lizard" => ['paper', 'spock'],
+                  "spock" => ['rock', 'scissors'] }
 
 def prompt(message)
   puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  VALID_CHOICES[first].include?(second)
 end
 
 def display_result(player, computer)
@@ -20,27 +22,53 @@ def display_result(player, computer)
   end
 end
 
+def valid_continue?
+  answer = ''
+  loop do
+    answer = gets.chomp
+    break if ['y', 'n'].include?(answer.downcase)
+    prompt "invalid_continue"
+  end
+  answer
+end
+
+def another_game?
+  prompt "Do you want to play another game?"
+  answer = valid_continue?
+  if answer.downcase == 'n'
+    false
+  else
+    clear_screen()
+    prompt "again"
+    true
+  end
+end
+
+def clear_screen
+  system("clear") || system("cls")
+end
+
+clear_screen()
+prompt "Welcome to Rock Paper Scissors Spock! Let's play!"
+
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Choose one: #{VALID_CHOICES.keys.join(', ')}")
     choice = gets.chomp
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.keys.include?(choice)
       break
     else
       prompt("That's not a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.keys.sample
 
   puts "You chose: #{choice}; Computer chose: #{computer_choice}"
 
   display_result(choice, computer_choice)
-
-  prompt "Do you want to play again?"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  break unless another_game?
 end
 
 prompt "Thank you for playing. Good bye!"
