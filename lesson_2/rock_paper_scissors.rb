2d67c33e2ae8d46c3534a91563dd302f12e55987
Anne Tiotuico <amtiotuico@gmail.com>
2020-09-10 03:16:47 +0000

    added functionality to accept abbreviation choices

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,11 +1,55 @@
+require 'yaml'
+MESSAGES = YAML.load_file('rock_paper_scissors.yml')
+LANGUAGE = 'en'
+
+ABBREVIATIONS = { "r" => "rock",
+                  "p" => "paper",
+                  "sc" => "scissors",
+                  "l" => "lizard",
+                  "sp" => "spock" }
+
 VALID_CHOICES = { "rock" => ['scissors', 'lizard'],
                   "paper" => ['rock', 'spock'],
                   "scissors" => ['paper', 'lizard'],
                   "lizard" => ['paper', 'spock'],
-                  "spock" => ['rock', 'scissors'] }
+                  "spock" => ['rock', 'scissors'],
+                  "r" => ['scissors', 'lizard'],
+                  "p" => ['rock', 'spock'],
+                  "sc" => ['paper', 'lizard'],
+                  "l" => ['paper', 'spock'],
+                  "sp" => ['rock', 'scissors'] }
 
-def prompt(message)
+scores = { "player" => 0, "computer" => 0 }
+
+def messages(message, lang = 'en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key)
+  message = messages(key, LANGUAGE)
   puts("=> #{message}")
+end
+
+def valid_choice
+  choice = ''
+  loop do
+    prompt "choices"
+    choice = gets.chomp.downcase
+    if VALID_CHOICES.keys.include?(choice)
+      break
+    else
+      prompt "invalid_choice"
+    end
+  end
+  choice.downcase
+end
+
+def display_choices(choice, computer_choice)
+  if ABBREVIATIONS.keys.include?(choice)
+    choice = ABBREVIATIONS[choice]
+  end
+  puts format(messages("player_choice"), choice: choice)
+  puts format(messages("computer_choice"), computer_choice: computer_choice)
 end
 
 def win?(first, second)
@@ -14,15 +58,41 @@ end
 
 def display_result(player, computer)
   if win?(player, computer)
-    prompt "You won!"
+    prompt "player_won"
   elsif win?(computer, player)
-    prompt "Computer won!"
+    prompt "computer_won"
   else
-    prompt "It's a tie!"
+    prompt "tie"
   end
 end
 
-def valid_continue?
+def get_score(scores, player, computer)
+  if win?(player, computer)
+    scores['player'] += 1
+  elsif win?(computer, player)
+    scores['computer'] += 1
+  end
+end
+
+def display_score(scores)
+  puts format(messages("display_score"),
+              player_score: scores['player'],
+              computer_score: scores['computer'])
+end
+
+def display_winner(scores)
+  if scores['player'] == 5
+    prompt "player_grand_winner"
+  elsif scores ['computer'] == 5
+    prompt "computer_grand_winner"
+  end
+end
+
+def game_over(scores)
+  scores['player'] == 5 || scores['computer'] == 5
+end
+
+def continue
   answer = ''
   loop do
     answer = gets.chomp
@@ -33,8 +103,8 @@ def valid_continue?
 end
 
 def another_game?
-  prompt "Do you want to play another game?"
-  answer = valid_continue?
+  prompt "another_game"
+  answer = continue
   if answer.downcase == 'n'
     false
   else
@@ -44,31 +114,33 @@ def another_game?
   end
 end
 
+def reset_score(scores)
+  scores['player'] = 0
+  scores['computer'] = 0
+end
+
 def clear_screen
   system("clear") || system("cls")
 end
 
 clear_screen()
-prompt "Welcome to Rock Paper Scissors Spock! Let's play!"
+prompt "welcome"
 
 loop do
-  choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.keys.join(', ')}")
-    choice = gets.chomp
-    if VALID_CHOICES.keys.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+    choice = valid_choice
+    computer_choice = ABBREVIATIONS.values.sample
+    clear_screen()
+    display_choices(choice, computer_choice)
+
+    display_result(choice, computer_choice)
+    get_score(scores, choice, computer_choice)
+    display_score(scores)
+    display_winner(scores)
+    break if game_over(scores)
   end
-
-  computer_choice = VALID_CHOICES.keys.sample
-
-  puts "You chose: #{choice}; Computer chose: #{computer_choice}"
-
-  display_result(choice, computer_choice)
+  reset_score(scores)
   break unless another_game?
 end
 
-prompt "Thank you for playing. Good bye!"
+prompt "goodbye"
