b030be44f4ab0834c1b561ba898b41312d00b8c4
Anne Tiotuico <amtiotuico@gmail.com>
2020-09-11 03:13:36 +0000

    modified program to ask for name, Teddy if no name entered

--- small_problems/easy2/how_old_is_teddy.rb
+++ small_problems/easy2/how_old_is_teddy.rb
@@ -1,3 +1,12 @@
+puts "What's your name?"
+name = gets.chomp
 age = rand(20..200)
+if name.empty?
+  puts "Teddy is #{age} years old!"
+else
+  puts "#{name} is #{age} years old!"
+end
 
-puts "Teddy is #{age} years old!"
+
+
+
