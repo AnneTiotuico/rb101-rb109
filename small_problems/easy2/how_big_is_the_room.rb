a4df108b89c84cf476001a1df248ac848e98a011
Anne Tiotuico <amtiotuico@gmail.com>
2020-09-11 03:31:19 +0000

    ask for input in feet and display sqft, sqin and sqcm

--- small_problems/easy2/how_big_is_the_room.rb
+++ small_problems/easy2/how_big_is_the_room.rb
@@ -1,12 +1,15 @@
-SQMETERS_TO_SQFEET = 10.7639
+SQFEET_TO_SQIN = 144
+SQFEET_TO_SQCM = 929.03
 
-puts "Enter the length of the room in meters:"
+puts "Enter the length of the room in feet:"
 length = gets.chomp.to_f
-puts "Enter the width of the room in meters:"
+puts "Enter the width of the room in feet:"
 width = gets.chomp.to_f
 
-square_meters = (length * width).round(2)
-square_feet = (square_meters * SQMETERS_TO_SQFEET).round(2)
+square_feet = (length * width).round(2)
+square_in = (square_feet * SQFEET_TO_SQIN).round(2)
+square_cm = (square_feet * SQFEET_TO_SQCM).round(2)
 
-puts "The area of the room is #{square_meters} square meters (#{square_feet} square feet)."
+puts "The area of the room is #{square_feet} square feet " + \
+      "(#{square_in} square inches, #{square_cm} square centimeters})."
 
