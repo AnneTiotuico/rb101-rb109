d6c1c71565e2212a78686ec85ff0c05a1b1d7806
Anne Tiotuico <amtiotuico@gmail.com>
2020-10-16 02:39:43 +0000

    refactored and cleaned code

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,5 +1,14 @@
+INITIAL_MARKER = ' '
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+                [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+# rubocop: disable Metrics/AbcSize
 def display_board(brd)
-  clear_screen()
+  clear_screen
   puts ""
   puts "1    |2    |3"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -14,14 +23,15 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop: enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' '} #{1=> ' ', 2=> ' '...etc}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
-def update_board(board, marker, position)
+def update_board!(board, marker, position)
   board[position] = marker
   display_board(board)
 end
@@ -29,11 +39,11 @@ end
 def choose_marker(board)
   marker = ''
   loop do
-    puts "Choose your marker: X or O"
+    prompt "Choose your marker: X or O"
     marker = gets.chomp.upcase
     break if ['X', 'O'].include?(marker)
     display_board(board)
-    puts "Please enter a valid marker."
+    prompt "Please enter a valid marker."
   end
   marker
 end
@@ -42,88 +52,80 @@ def assign_comp_marker(player_marker)
   player_marker == 'X' ? 'O' : 'X'
 end
 
+def empty_squares(board)
+  board.keys.select { |num| board[num] == INITIAL_MARKER }
+end
+
 def players_turn(board, marker)
   position = ''
   loop do
-    puts "Which box would you like to mark? (1-9)"
-    position = gets.chomp
-    break if ('1'..'9').include?(position) && board[position.to_i] == ' '
+    prompt "Choose a square. (#{empty_squares(board).join(', ')})"
+    position = gets.chomp.to_i
+    break if empty_squares(board).include?(position)
     display_board(board)
-    puts "Invalid box, please choose an empty box between 1-9."
+    prompt "Invalid square, please choose an empty square."
   end
-  update_board(board, marker, position.to_i)
+  update_board!(board, marker, position)
 end
 
 def computers_turn(board, marker)
-  position = 0
-  loop do
-    position = (1..9).to_a.sample
-    break if board[position] == ' '
-  end
-  update_board(board, marker, position)
-  puts "Computer played box #{position}. Your turn."
+  position = empty_squares(board).sample
+  update_board!(board, marker, position)
+  prompt "Computer (#{marker}) played square #{position}."
 end
 
 def board_full?(board)
-  board.values.all? { |marked| ['X', 'O'].include?(marked) }
+  empty_squares(board).empty?
 end
 
-def three_in_a_row?(board)
-  win_row = [[1, 2, 3], [4,5,6], [7,8,9], [1, 4, 7],
-          [2, 5, 8], [3, 6, 9], [1, 5, 9], [3,5,7]]
-  win_row.map do |row|
-    return row if row.all? {|num| board[num] == 'X'}
-    return row if row.all? {|num| board[num] == 'O'}
-  end.any?
-  return false
+def winner?(board, player_marker, comp_marker)
+  WINNING_LINES.each do |row|
+    return "Player" if row.all? { |num| board[num] == player_marker }
+    return "Computer" if row.all? { |num| board[num] == comp_marker }
+  end
+  false
 end
 
-def determine_winner(board, winning_row)
-  if winning_row
-    if board[winning_row[0]] == 'X'
-      puts "X wins!"
-    else
-      puts "O wins!"
-    end
+def determine_winner(winner)
+  if winner
+    prompt "#{winner} won!"
   else
-    puts "Tie!"
+    prompt "It's a Tie!"
   end
 end
 
-def play_again?(board)
+def play_again?
   answer = ''
   loop do
-    puts "Do you want to play again? (y/n)"
+    prompt "Do you want to play again? (y/n)"
     answer = gets.chomp.downcase
     break if ['y', 'n'].include?(answer)
-    puts "Invalid, please enter 'y' or 'n'."
+    prompt "Invalid, please enter 'y' or 'n'."
   end
-  answer == 'y' ? true : false
+  answer == 'y'
 end
 
 def clear_screen
   system("clear") || system("cls")
 end
 
-#main game
-puts "Welcome! Let's play some tic tac toe!"
-
-
-#main loop
+# main game
+prompt "Welcome! Let's play some tic tac toe!"
+# main loop
 loop do
   board = initialize_board # blank board
   display_board(board) # output board
-  #choose markers
+  # choose markers
   player_marker = choose_marker(board)
   comp_marker = assign_comp_marker(player_marker)
   loop do
-    break if three_in_a_row?(board)
+    break if winner?(board, player_marker, comp_marker)
     players_turn(board, player_marker)
-    break if three_in_a_row?(board) || board_full?(board)
+    break if winner?(board, player_marker, comp_marker) || board_full?(board)
     computers_turn(board, comp_marker)
   end
-  determine_winner(board, three_in_a_row?(board))
-  break unless play_again?(board)
+  determine_winner(winner?(board, player_marker, comp_marker))
+  break unless play_again?
 end
 
-puts "Goodbye, thanks for playing!"
+prompt "Goodbye, thanks for playing tic tac toe!"
